ae60a12a5c9cc504395c965f4ee85ae9c7dda042
HIROKI <dramaticbbb@gmail.com>
2023-07-23 19:48:38 +0900

    プロフィール関連ページを追加しました

--- routes/profile.js
+++ routes/profile.js
@@ -1,29 +1,38 @@
 const express = require("express");
-const app = express();
+// const app = express();
 const router = express.Router();
 
 // router.use(mylogger);
 
 // var express = require('express');
 // module.exports = router;
-app.set('view engine', 'ejs');
+// app.set('view engine', 'ejs');
 
 router.get("/", mylogger, (req, res) => {
   res.render("profile");
 });
+// 作品紹介へのルーティング
+router.get("/works", (req, res) => {
+  res.render("works");
+});
+// 学習紹介へのルーティング
+router.get("/learningReview", (req, res) => {
+  res.render("learningReview");
+});
+// 今後の目標へのルーティング
 router.get("/futureGoals", (req, res) => {
   res.render("futureGoals");
 });
 
-router.get("/:id", (req, res) => {
-  res.send(`${req.params.id}のユーザー情報を取得しました。`);
-});
-router.post("/:id", (req, res) => {
-  res.send(`${req.params.id}のユーザー情報を取得しました。`);
-});
-router.delete("/:id", (req, res) => {
-  res.send(`${req.params.id}のユーザー情報を取得しました。`);
-});
+// router.get("/:id", (req, res) => {
+//   res.send(`${req.params.id}のユーザー情報を取得しました。`);
+// });
+// router.post("/:id", (req, res) => {
+//   res.send(`${req.params.id}のユーザー情報を取得しました。`);
+// });
+// router.delete("/:id", (req, res) => {
+//   res.send(`${req.params.id}のユーザー情報を取得しました。`);
+// });
 
 // ミドルウェア
 function mylogger(req, res, next){
--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+// server.js
+
 const express = require("express");
 const app = express();
 const http = require("http");
@@ -5,17 +7,18 @@ const server = http.createServer(app);
 const io = require("socket.io")(server);
 const userRouter = require("./routes/user");
 const profileRouter = require("./routes/profile");
-const path =require("path");
+const path = require("path");
+
 // PORT を指定
 const PORT = 3030;
 
 app.get("/chat", (req, res) => {
-    res.setHeader("Content-Type", "text/html; charset=utf-8");
-    res.sendFile(path.join(__dirname, "views", "chatApp.ejs"));
+  res.setHeader("Content-Type", "text/html");
+  res.sendFile(path.join(__dirname, "views", "chatApp.ejs"));
 });
-  
+
 io.on("connection", (socket) => {
-    console.log("ユーザーが接続しました");
+  console.log("ユーザーが接続しました");
 });
 
 app.use(express.static("views"));
@@ -26,11 +29,12 @@ app.set('view engine', 'ejs');
 app.use("/user", userRouter);
 app.use("/profile", profileRouter);
 
-server.listen(PORT, () => {
-    console.log("サーバーが起動しました。");
-});
-// 他のミドルウェアの後にエラーハンドリングミドルウェアを追加（最後に配置する）
+// 他のミドルウェアの後にエラーハンドリングミドルウェアを追加
 app.use(function (err, req, res, next) {
-    console.error(err.stack);
-    res.status(500).send("Internal Server Error");
+  console.error(err.stack);
+  res.status(500).send("Internal Server Error");
+});
+
+server.listen(PORT, () => {
+  console.log("サーバーが起動しました。");
 });
